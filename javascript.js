1275854b66d7bf49d5c583de0a16604c106b4eff
Edward <147109691+edward5292411@users.noreply.github.com>
2023-11-16 16:47:15 +1300

    Create a function called getComputerChoice that will randomly return either 'Rock', 'Paper' or 'Scissors'

--- javascript.js
+++ javascript.js
@@ -4,3 +4,9 @@
 /* Write a playRound function to compare player selection with computer selection */
 /* Return the results of playRound, not console.log() them */
 /* Put previous playRound function inside a game function to play 5 round game that keeps score and reports winner or loser at the end */
+
+let arr = ["rock", "paper", "scissor"];
+function getComputerChoice(){
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+console.log(getComputerChoice());
