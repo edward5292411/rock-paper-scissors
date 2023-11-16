081e9ca9c31e78f089af894322b6439d11d8d8cd
Edward <147109691+edward5292411@users.noreply.github.com>
2023-11-16 17:36:33 +1300

    Change 'scissor' to 'scissors' + Fix SyntaxError as forgot to put ending curly braces

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@
 /* Return the results of playRound, not console.log() them */
 /* Put previous playRound function inside a game function to play 5 round game that keeps score and reports winner or loser at the end */
 
-let arr = ["rock", "paper", "scissor"];
+let arr = ["rock", "paper", "scissors"];
 function getComputerChoice(){
     return arr[Math.floor(Math.random() * arr.length)];
 }
@@ -13,20 +13,21 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     if(playerSelection === computerSelection){
         return "A Tie!";
-    } else if((playerSelection = "\rock\i") && (computerSelection = "scissor")){
-        return "You Win! Rock beats Scissor";
+    } else if((playerSelection = "\rock\i") && (computerSelection = "scissors")){
+        return "You Win! Rock beats Scissors";
     } else if((playerSelection = "\paper\i") && (computerSelection = "rock")){
         return "You Win! Paper beats Rock";
     } else if((playerSelection = "\scissor\i") && (computerSelection = "paper")){
-        return "You Win! Scissor beats Paper";
+        return "You Win! Scissors beat Paper";
     } else if((playerSelection = "\scissor\i") && (computerSelection = "rock")){
-        return "You lose! Rock beats Scissor";
+        return "You lose! Rock beats Scissors";
     } else if((playerSelection = "\rock\i") && (computerSelection = "paper")){
         return "You lose! Paper beats Rock";
     } else {
-        return "You lose! Scissor beats Paper";
+        return "You lose! Scissors beat Paper";
+    }
 }
 
-let playerSelection = prompt("What's your selection? Enter either Paper, Rock or Scissors", "");
+let playerSelection = "rock";
 let computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
