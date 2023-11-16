c6da14c1fbf6fe21d47b52aac5dbf13bd734e9e0
Edward <147109691+edward5292411@users.noreply.github.com>
2023-11-16 16:25:25 +1300

    Write out pseudocode for the game play

---
+++ javascript.js
@@ -0,0 +1,6 @@
+/* Create variables for player selection and computer selection */
+/* User inputs rock, paper or scissor via prompt () */
+/* Return randomly computer selection of rock, paper pr scissor */
+/* Write a playRound function to compare player selection with computer selection */
+/* Return the results of playRound, not console.log() them */
+/* Put previous playRound function inside a game function to play 5 round game that keeps score and reports winner or loser at the end */
