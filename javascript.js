40718bfcab1b523b36303b084198a78e81c05f76
Edward <147109691+edward5292411@users.noreply.github.com>
2023-11-17 16:45:03 +1300

    add buttons and <p> to html and wrap all functions inside game()

--- javascript.js
+++ javascript.js
@@ -3,31 +3,40 @@
 /* Return randomly computer selection of rock, paper pr scissor */
 /* Write a playRound function to compare player selection with computer selection */
 /* Return the results of playRound, not console.log() them */
-/* Put previous playRound function inside a game function to play 5 round game that keeps score and reports winner or loser at the end */
+/* Put previous playRound function inside a game function to play a 5 round game that keeps score and reports winner or loser at the end */
 
-let arr = ["rock", "paper", "scissors"];
-function getComputerChoice(){
-    return arr[Math.floor(Math.random() * arr.length)];
-}
 
-function playRound(playerSelection, computerSelection){
-    if(playerSelection === computerSelection){
-        return "A Tie!";
-    } else if((playerSelection = "\rock\i") && (computerSelection = "scissors")){
-        return "You Win! Rock beats Scissors";
-    } else if((playerSelection = "\paper\i") && (computerSelection = "rock")){
-        return "You Win! Paper beats Rock";
-    } else if((playerSelection = "\scissor\i") && (computerSelection = "paper")){
-        return "You Win! Scissors beat Paper";
-    } else if((playerSelection = "\scissor\i") && (computerSelection = "rock")){
-        return "You lose! Rock beats Scissors";
-    } else if((playerSelection = "\rock\i") && (computerSelection = "paper")){
-        return "You lose! Paper beats Rock";
-    } else {
-        return "You lose! Scissors beat Paper";
+let game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+
+    let playGame = () => {
+        let playerSelection = prompt("What is your selection? Type either Rock, Paper or Scissors.", "");
+        let computerSelection = getComputerChoice();
     }
-}
 
-let playerSelection = "rock";
-let computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+    function getComputerChoice(){
+        let arr = ["rock", "paper", "scissors"];
+        return arr[Math.floor(Math.random() * arr.length)];
+    }
+    
+    function playRound(playerSelection, computerSelection){
+        if(playerSelection === computerSelection){
+            return "A Tie!";
+        } else if((playerSelection = "\rock\i") && (computerSelection = "scissors")){
+            return "You Win!";
+        } else if((playerSelection = "\paper\i") && (computerSelection = "rock")){
+            return "You Win!";
+        } else if((playerSelection = "\scissors\i") && (computerSelection = "paper")){
+            return "You Win!";
+        } else if((playerSelection = "\scissors\i") && (computerSelection = "rock")){
+            return "You lose!";
+        } else if((playerSelection = "\rock\i") && (computerSelection = "paper")){
+            return "You lose!";
+        } else {
+            return "You lose!";
+        }
+    }
+
+}
